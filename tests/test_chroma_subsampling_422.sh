9b533691b5d55f9e8f16cd89a9bdb867a624e432
Marko Viitanen <fador@iki.fi>
2026-08-06 10:22:08 +0300

    Separate 4:4:4 and 4:2:2 tests since they are taking a lot of time

--- tests/test_chroma_subsampling_422.sh
+++ tests/test_chroma_subsampling_422.sh
@@ -10,10 +10,10 @@ common_args='--preset=veryslow -p0 --threads=2 --wpp'
 # Skip valgrind tests if KVZ_TEST_VALGRIND is not set to 1
 if [ "${KVZ_TEST_CHROMASUBSAMPLING:-0}" != '0' ]; then
 
-  valgrind_test_444 264x130 10 $common_args --gop=8 -p0 --owf=1
-  valgrind_test_444 264x130 10 $common_args --gop=16 -p0 --owf=1 --cross-comp-pred
+  valgrind_test_422 264x130 10 $common_args --gop=8 -p0 --owf=1
+  valgrind_test_422 264x130 10 $common_args --gop=16 -p0 --owf=1
 
   # Do more extensive tests in a private gitlab CI runner
-  if [ ! -z ${GITLAB_CI+x} ];then valgrind_test_444 264x130 40 $common_args --gop=8 -p32 --owf=4 --no-open-gop; fi
+  if [ ! -z ${GITLAB_CI+x} ];then valgrind_test_422 264x130 40 $common_args --gop=8 -p32 --owf=4 --no-open-gop; fi
 
 fi
